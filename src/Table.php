58a7dc5777a459cb9ef192bfcd85371b72cdbc3a
Royall Spence <royall@royall.us>
2020-09-17 10:54:23 -0400

    Log savings after alter table

--- src/Table.php
+++ src/Table.php
@@ -43,12 +43,13 @@ class Table
             $this->dataFree / $this->dataLength);
     }
 
-    public function LogFinish(): string
+    public function LogFinish(int $kbytes): string
     {
-        return sprintf("%s Finished shrinking table %s.%s\n",
+        return sprintf("%s Finished shrinking table %s.%s. Saved %d kilobytes\n",
             date(DATE_ISO8601),
             $this->db,
-            $this->name);
+            $this->name,
+            $kbytes);
     }
 
     public function ShouldShrink(float $ratio): bool
@@ -61,14 +62,32 @@ class Table
 
     /**
      * @param DataObject $spdo
+     * @return int Number of bytes saved
      * @throws Exception
      */
-    public function Shrink(DataObject $spdo): void
+    public function Shrink(DataObject $spdo): int
     {
         try {
             $spdo->prepare("ALTER TABLE {$this->db}.{$this->name} ENGINE=\"InnoDB\";")->execute();
         } catch (Exception $e) {
             throw new Exception("Failed to shrink table {$this->db}.{$this->name}", 0, $e);
         }
+
+        try {
+            $dataLength = $spdo->prepare(
+                "SELECT DATA_LENGTH
+FROM information_schema.tables
+WHERE TABLE_SCHEMA LIKE ?
+AND TABLE_NAME LIKE ?
+AND ENGINE LIKE 'InnoDB'
+AND DATA_FREE > 0;")->execute([$this->db, $this->name])->fetch(\PDO::FETCH_ASSOC)['DATA_LENGTH'];
+        } catch (Exception $e) {
+            return 0;
+        }
+
+        $oldSize = $this->dataLength;
+        $this->dataLength = $dataLength;
+
+        return $dataLength - (int)$oldSize;
     }
 }
